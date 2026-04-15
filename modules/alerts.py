3dd237f9e8c798435cd940ca7cefae446128696a
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-15 11:08:43 -0600

    Update alerts.py

--- modules/alerts.py
+++ modules/alerts.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 def show_alerts(df):
-    st.subheader("Alumnos en riesgo")
+    st.subheader("🚨 Alumnos en riesgo")
 
-    alertas = df[df["Estado"] == 0]
+    # 🔥 Nivel 2: bajo rendimiento
+    alertas = df[(df["Estado"] == 0) | (df["Puntos"] <= 3)]
 
     if alertas.empty:
         st.success("Sin alertas")
