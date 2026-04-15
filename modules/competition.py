1cd02cb6b1a571f9693e6a24ed47bed9edd86d0e
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-15 14:47:13 -0600

    Update competition.py

--- modules/competition.py
+++ modules/competition.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+def show_ranking(df):
+    st.subheader("🥇 Top Alumnos")
+
+    ranking = df.sort_values(by="Puntos", ascending=False).head(5)
+
+    for i, row in ranking.iterrows():
+        st.write(f"{row['Alumno']} → ⭐ {row['Puntos']}")
+
 def show_competition(df):
     st.subheader("🏆 Marcador por Equipos")
 
