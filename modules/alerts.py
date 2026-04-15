3cbbf2d3c5d31b623b50bbf373bf6c5da26158e5
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-15 10:10:40 -0600

    Update alerts.py

--- modules/alerts.py
+++ modules/alerts.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 def show_alerts(df):
-    st.subheader("🚨 Alumnos en riesgo")
+    st.subheader("Alumnos en riesgo")
 
     alertas = df[df["Estado"] == 0]
 
     if alertas.empty:
-        st.success("No hay alumnos en riesgo 🎉")
+        st.success("Sin alertas")
     else:
         st.dataframe(alertas, use_container_width=True)
