8091807e35dec5567028a90e3879bf29808be1e7
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-15 10:10:08 -0600

    Update metrics.py

--- modules/metrics.py
+++ modules/metrics.py
@@ -4,9 +4,9 @@ def show_metrics(df):
     col1, col2, col3 = st.columns(3)
 
     total = df["Alumno"].nunique()
-    activos = df["Estado"].sum()
-    riesgo = len(df[df["Estado"] == 0])
+    activos = int(df["Estado"].sum())
+    riesgo = int((df["Estado"] == 0).sum())
 
-    col1.metric("👥 Alumnos", total)
-    col2.metric("✅ Activos", activos)
-    col3.metric("🚨 Riesgo", riesgo)
+    col1.metric("Alumnos", total)
+    col2.metric("Activos", activos)
+    col3.metric("Riesgo", riesgo)
