97ef3ef4781d6d59b6e9ad2420961aebcf6d7ab3
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-21 08:29:59 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,23 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit as st
 
-# Inicialización segura
 if not firebase_admin._apps:
-
-    cred = credentials.Certificate({
-        "type": st.secrets["firebase"]["type"],
-        "project_id": st.secrets["firebase"]["project_id"],
-        "private_key_id": st.secrets["firebase"]["private_key_id"],
-        "private_key": st.secrets["firebase"]["private_key"].replace("\\n", "\n"),
-        "client_email": st.secrets["firebase"]["client_email"],
-        "client_id": st.secrets["firebase"]["client_id"],
-        "auth_uri": st.secrets["firebase"]["auth_uri"],
-        "token_uri": st.secrets["firebase"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
-    })
-
+    cred = credentials.Certificate("firebase_key.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
