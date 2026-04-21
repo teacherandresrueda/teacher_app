9e127b3d465dcb1df6be041d60a1ad9ae7bc8a0f
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-21 08:00:05 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -14,3 +14,25 @@ if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+
+# ---------------- ALUMNOS ----------------
+
+def add_student(teacher, name):
+    db.collection("students").add({
+        "teacher": teacher,
+        "name": name,
+        "points": 0
+    })
+
+
+def get_students(teacher):
+    docs = db.collection("students").where("teacher", "==", teacher).stream()
+    return [doc.to_dict() | {"id": doc.id} for doc in docs]
+
+
+def add_points(student_id, points):
+    ref = db.collection("students").document(student_id)
+    student = ref.get().to_dict()
+
+    new_points = student["points"] + points
+    ref.update({"points": new_points})
