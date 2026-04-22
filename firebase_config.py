8431c3225589b98a831dea417b4b4fcf4779491c
teacherandresrueda <teacherandresrueda@gmail.com>
2026-04-22 14:58:47 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
