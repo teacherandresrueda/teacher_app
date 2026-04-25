2b8a68c43a23db198f8b8d0cb89f4a041849c478
Andrés Emmanuel Rueda Alor <teacherandresrueda@gmail.com>
2026-04-24 22:23:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import hashlib
 
-st.write(type(st.secrets.get("firebase")))
-st.write(st.secrets.get("firebase"))
-
 # ---------------- FIREBASE INIT ----------------
 if not firebase_admin._apps:
  cred = credentials.Certificate(dict(st.secrets["firebase"]))
